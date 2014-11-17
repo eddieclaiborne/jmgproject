21f63c06309e3de5bcb36a7ecbcca15846fff932
McLane Ballenger <mclaneballenger@dhcp-10-201-215-76.tamulink.tamu.edu>
2014-11-17 03:15:00 -0600

    Initial Commit

---
+++ jmgproject/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  jmgproject
+//
+//  Created by McLane Ballenger on 11/17/14.
+//  Copyright (c) 2014 McLane Ballenger. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
